21de98cd1e95fb9979d80d10080f5dc663b8a916
vinayv <vinayv@fico.com>
2025-11-28 14:34:55 +0530

    update in ec2 instance tf

--- spacelift/ec2.tf
+++ spacelift/ec2.tf
@@ -1,9 +1,24 @@
-resource "aws_instance" "sample" {
-    ami = "ami-00d2efe5bc0683614"
-    instance_type = "t2.micro"
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-    tags = {
-        "Name" = "Sample"
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
+resource "aws_instance" "sample" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "sample"
+  }
+}
